031d13a5cf62d19a0e0cfdc2ab5edd2c33aeac9c
Shaun S Dsilva <dsilvashaun.edu@gmail.com>
2026-09-11 20:32:19 +0530

    New Temp Replacement
    A test replacement

---
+++ Website Code (Project.0)/JavaScripts/script.js
@@ -0,0 +1,11 @@
+// Verification that the JS file is connected to the HTML file
+
+console.log("JavaScript is working!");
+
+// Early Pop-Up modal
+
+function closePopup() {
+
+    document.getElementById("popup").style.display = "none";
+
+}
